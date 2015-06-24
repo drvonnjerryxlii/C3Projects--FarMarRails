3fa079c98466f510bfd0a817d69bdc946d904e32
Jeri <drvonnjerryxlii@gmail.com>
2015-06-23 18:16:26 -0700

    user can view specific markets and see attributes and vendors now

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,6 +1,11 @@
 class MarketsController < ApplicationController
-
   def index
     @markets = Market.all
+  end
+
+  def show
+    id = params.permit(:id)[:id]
+    @market = Market.find(id)
+    @vendors = @market.vendors
   end
 end
